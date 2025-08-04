8d1ce3a4c150c0f31856c784f5c89bb3f04c7ef1
charlesbrall <66047110+charlesbrall@users.noreply.github.com>
2025-08-03 22:59:34 -0300

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,50 @@
+import type { Metadata } from 'next';
+import './globals.css';
+import { Toaster } from "@/components/ui/toaster";
+import { cn } from '@/lib/utils';
+import { Poppins } from 'next/font/google';
+import Script from 'next/script';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '600', '700', '900']
+});
+
+export const metadata: Metadata = {
+  title: 'Desafio 7 Dias Sem Lactose',
+  description: 'Descubra como é fácil viver sem lactose, comendo com prazer!',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR" className="scroll-smooth">
+      <body className={cn(poppins.className, "antialiased")}>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1309953657154564');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=1309953657154564&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
+}
